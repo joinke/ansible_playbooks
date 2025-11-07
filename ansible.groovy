9aa48497bae06eee56a94c65c76c2dffd5fcdd1a
Joinke <38005255+joinke@users.noreply.github.com>
2025-11-07 20:20:34 +1100

    Update Ansible playbook and inventory paths

--- ansible.groovy
+++ ansible.groovy
@@ -7,15 +7,15 @@ pipeline {
         stage('Hello') {
             steps {
                 echo 'Hello World'
-                sh 'cat /opt/ansible.yml'
-                sh 'cat /opt/ansible_hosts'
+                sh 'cat playbooks/list.yml'
+                sh 'cat inventories/ansible_hosts'
                 ansiblePlaybook becomeUser: 'wanpen', 
                                 colorized: true, 
                                 credentialsId: '00b69538-5290-4373-a385-c2e59e5a4d9f', 
                                 disableHostKeyChecking: true, 
                                 installation: 'Ansible', 
-                                inventory: '/opt/ansible_hosts', 
-                                playbook: '/opt/ansible.yml',
+                                inventory: 'inventories/ansible_hosts', 
+                                playbook: 'playbooks/list.yml',
                                 extraVars: [fetch_dest: "${WORKSPACE}/fetched/"]
             }
         }
