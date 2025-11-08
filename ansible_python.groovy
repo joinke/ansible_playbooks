3776ea30ad12b3fd217efe827347be9785120701
Joinke <38005255+joinke@users.noreply.github.com>
2025-11-08 13:18:22 +1100

    Create ansible_python.groovy

---
+++ ansible_python.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any
+    options {
+        ansiColor('xterm')
+    }
+    stages {
+      stage('Run Ansible via Python') {
+        steps {
+          sshagent(['00b69538-5290-4373-a385-c2e59e5a4d9f']) {
+            sh 'python3 -u scripts/run_ansible.py'
+          }
+        }
+      }
+    }
+    post {
+        always {
+            archiveArtifacts artifacts: 'fetched/**/*'
+        }
+    }
+}
