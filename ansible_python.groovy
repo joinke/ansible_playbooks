73f8604cead6f760b86623f39c3b5a5c8ee0d717
Joinke <38005255+joinke@users.noreply.github.com>
2025-11-08 13:19:24 +1100

    Change script path from run_ansible.py to ansible_python.py

--- ansible_python.groovy
+++ ansible_python.groovy
@@ -7,7 +7,7 @@ pipeline {
       stage('Run Ansible via Python') {
         steps {
           sshagent(['00b69538-5290-4373-a385-c2e59e5a4d9f']) {
-            sh 'python3 -u scripts/run_ansible.py'
+            sh 'python3 -u ansible_python.py'
           }
         }
       }
