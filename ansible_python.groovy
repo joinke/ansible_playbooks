80a9d669fc4b5bed23f55e9be1ca584147eb69bf
Joinke <38005255+joinke@users.noreply.github.com>
2025-11-08 21:49:37 +1100

    Update ansible_python.groovy

--- ansible_python.groovy
+++ ansible_python.groovy
@@ -85,6 +85,7 @@ pipeline {
   environment {
     HOST_LIST = '192.168.70.175,192.168.70.193'
     OPERATION = "${params.OPERATION}"
+    ENVS = env['\u200B']
   }
   stages {
     stage('Run Ansible via Python') {
@@ -93,7 +94,7 @@ pipeline {
                                            keyFileVariable: 'SSH_KEY',
                                            usernameVariable: 'SSH_USER')]) {
           sh '''
-            echo "🧩 Using SSH key from Jenkins: $SSH_KEY for user $SSH_USER and $OPERATION "
+            echo "🧩 Using SSH key from Jenkins: $SSH_KEY for user $SSH_USER and $OPERATION and $ENVS"
 
             # Run the Python wrapper (Ansible will use the key directly)
             python3 -u $OPERATION
