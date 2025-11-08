cba5946cb2857ba45c8a39d3e91b65fa2b641493
Joinke <38005255+joinke@users.noreply.github.com>
2025-11-08 16:45:15 +1100

    Update ssh_runner_class.py

--- ssh_runner_class.py
+++ ssh_runner_class.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 
 class SSHRunner:
-    def __init__(self, hosts=None, user=None, key_file=None, inventory_file=None):
+    def __init__(self, hosts=None, user=None, key_file=None, inventory_file='hostsfile'):
         """
         hosts: optional list of host IPs / hostnames
         user: SSH username (optional, defaults to env SSH_USER)
