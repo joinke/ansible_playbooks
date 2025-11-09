fd6076147a6802d92467b5bd67c9a8a9c28e6e67
Joinke <38005255+joinke@users.noreply.github.com>
2025-11-09 15:47:26 +1100

    Update ansible_python.groovy

--- ansible_python.groovy
+++ ansible_python.groovy
@@ -3,6 +3,11 @@ def envMap = [
     'UAT02': 'UAT02',
     'UAT03': 'UAT03'
 ]
+def compMap = [
+    'STP': 'STP',
+    'WB': 'WB',
+    'STPWB': 'ALL'
+]
 def getSelectedKeys(mymap, boolString) {
     def bools = boolString.split(',').collect { it.trim().toBoolean() }
     def keys = mymap.keySet().toList()
@@ -65,7 +70,6 @@ properties([
                     sandbox: true,
                     script: '''
                         def op = OPERATION?.trim()
-                        //def environmentmap = ${groovy.json.JsonOutput.toJson(envMap)}
                         def environmentmap = [
                             'UAT01': 'UAT01',
                             'UAT02': 'UAT02',
@@ -77,6 +81,44 @@ properties([
                         // Build checkbox list
                         def html = new StringBuilder()
                         environmentmap.each { value, label ->
+                            def checked = (value in defaultSelected) ? 'checked' : ''
+                            html.append("<label>")
+                            html.append("<input type='checkbox' name='value' value='${value}' ${checked}> ${label}")
+                            html.append("</label><br>")
+                        }
+
+                        return html.toString()
+                        } else {
+                          return ''
+                        }
+                    '''
+                ]
+            ]
+        ],
+        [
+            $class: 'DynamicReferenceParameter',
+            name: '\u200C',
+            choiceType: 'ET_FORMATTED_HTML',
+            omitValueField: true,
+            referencedParameters: 'COMPONENT',
+            script: [
+                $class: 'GroovyScript',
+                script: [
+                    $class: 'SecureGroovyScript',
+                    sandbox: true,
+                    script: '''
+                        def op = OPERATION?.trim()
+                        def compmap = [
+                            'STP': 'STP',
+                            'WB': 'WB',
+                            'STPWB': 'ALL'
+                        ]
+                        // Pre-select some options if needed
+                        def defaultSelected = ['main']
+                        if (op == 'ssh_runner.py') {
+                        // Build checkbox list
+                        def html = new StringBuilder()
+                        compmap.each { value, label ->
                             def checked = (value in defaultSelected) ? 'checked' : ''
                             html.append("<label>")
                             html.append("<input type='checkbox' name='value' value='${value}' ${checked}> ${label}")
@@ -104,20 +146,24 @@ pipeline {
     HOST_LIST = '192.168.70.175,192.168.70.193'
     OPERATION = "${params.OPERATION}"
     ENVS = "${params['\u200B']}"
+    COMPS = "${params['\u200C']}"
   }
   stages {
     stage('Run Ansible via Python') {
       steps {
         script {
-            selected = getSelectedKeys(envMap, ENVS)
-            echo "Selected keys: ${selected}"
-            env.SELECTEDENVS = "$selected"
+            environments = getSelectedKeys(envMap, ENVS)
+            echo "Selected environments: ${environments}"
+            env.SELECTEDENVS = "$environments"
+            components = getSelectedKeys(compMap, COMPS)
+            echo "Selected components: ${components}"
+            env.SELECTEDCOMP = "$components"
         }
         withCredentials([sshUserPrivateKey(credentialsId: '00b69538-5290-4373-a385-c2e59e5a4d9f',
                                            keyFileVariable: 'SSH_KEY',
                                            usernameVariable: 'SSH_USER')]) {
           sh '''
-            echo "🧩 Using SSH key from Jenkins: $SSH_KEY for user $SSH_USER and $OPERATION and $SELECTEDENVS"
+            echo "🧩 Using SSH key from Jenkins: $SSH_KEY for user $SSH_USER and $OPERATION and $SELECTEDENVS and $SELECTEDCOMP"
 
             # Run the Python wrapper (Ansible will use the key directly)
             python3 -u $OPERATION
