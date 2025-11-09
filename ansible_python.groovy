f186622f76a980afccdae3de5466accb20f83a61
Joinke <38005255+joinke@users.noreply.github.com>
2025-11-09 17:38:11 +1100

    Update ansible_python.groovy

--- ansible_python.groovy
+++ ansible_python.groovy
@@ -153,9 +153,9 @@ pipeline {
             environments = getSelectedKeys(envMap, ENVS)
             echo "Selected environments: ${environments}"
             env.SELECTEDENVS = "$environments"
-            components = getSelectedKeys(compMap, COMPS)
-            echo "Selected components: ${components}"
-            env.SELECTEDCOMP = "$components"
+            //components = getSelectedKeys(compMap, COMPS)
+            //echo "Selected components: ${components}"
+            env.SELECTEDCOMP = "$COMPS"
         }
         withCredentials([sshUserPrivateKey(credentialsId: '00b69538-5290-4373-a385-c2e59e5a4d9f',
                                            keyFileVariable: 'SSH_KEY',
