8cbf78e3e491193f6570bede87412a3f4a19a1a8
Joinke <38005255+joinke@users.noreply.github.com>
2025-11-10 23:08:32 +1100

    Update ansible_python.groovy

--- ansible_python.groovy
+++ ansible_python.groovy
@@ -274,12 +274,13 @@ properties([
                         def env = ENVIRONMENT
                         def site = SITE
                         def comp = COMPONENT
-                        println "${hostMap}"
                         def hosts = hostMap[env][site][comp]
-
                         def html = new StringBuilder("<select name='value'>")
+                        hosts.each { h ->
+                            html.append("<option value='${h}'>${h}</option>")
+                        }
                         html.append("</select>")
-
+        
                         return html.toString()
                     """
                 ]
