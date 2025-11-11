cf5aa53443bfabd0207401ec5822493aca58a811
Joinke <38005255+joinke@users.noreply.github.com>
2025-11-11 21:17:30 +1100

    Update ssh_runner.py

--- ssh_runner.py
+++ ssh_runner.py
@@ -2,6 +2,7 @@
 import os
 from ssh_runner_class import SSHRunner   # your SSHRunner class
 from environmenthosts import EnvironmentHosts
+from lookuphosts import HostResolver
 
 WORKSPACE = os.getenv("WORKSPACE", "/tmp")
 HOSTS = os.getenv("HOST_LIST", "")
@@ -12,6 +13,17 @@ runner = SSHRunner(
     hosts = HOSTS,
     max_workers=10
 )
+resolver = HostResolver()
+
+print(resolver.get_hosts(["UAT01"], "RCC", "STP"))
+# → hosta,hostb
+
+print(resolver.get_hosts(["UAT01"], "ALL", "STPWB"))
+# → hosta,hostb,hostc,hostd,hoste,hostf,hostg,hosth
+
+print(resolver.get_hosts(["UAT01","UAT02"], "BOTH", "WB"))
+# → hostc,hostd,hostg,hosth,hostk,hostl,hosto,hostp
+
 
 # Run any command, e.g., your Python script with arguments
 print(f"My environments are {SELECTEDENVS} and components {SELECTEDCOMP}")
