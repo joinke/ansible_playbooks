1891a03867206241f6bf593653a74b55b918be7c
Joinke <38005255+joinke@users.noreply.github.com>
2025-11-12 19:30:50 +1100

    Update lookuphosts.py

--- lookuphosts.py
+++ lookuphosts.py
@@ -28,7 +28,7 @@ class HostResolver:
         """
         envs: list of environments (e.g. ["UAT01","UAT02"])
         site: "RCC", "WSDC", or "ALL"/"BOTH"
-        comp: "STP", "WB", or "STPWB"
+        comp: "STP", "WB", or "ALL/BOTH"
         Returns: CSV string of hostnames
         """
     def get_hosts(self, envs, site, comp):
@@ -38,10 +38,10 @@ class HostResolver:
         hosts = []
         for env in envs:
             env_map = self.host_map.get(env, {})
-            sites_to_use = env_map.keys() if site in ("ALL", "BOTH") else [site]
+            sites_to_use = env_map.keys() if site in ("ALL") else [site]
             for s in sites_to_use:
                 comp_map = env_map.get(s, {})
-                comps_to_use = ["STP","WB"] if comp == "STPWB" else [comp]
+                comps_to_use = ["STP","WB"] if comp == "ALL" else [comp]
                 for c in comps_to_use:
                     hosts.extend(comp_map.get(c, []))
         return ",".join(sorted(set(hosts)))
