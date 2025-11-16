5c7715678b51fc63a7f7733c7722436e0cad6f78
Joinke <38005255+joinke@users.noreply.github.com>
2025-11-16 16:14:22 +1100

    Update copilotpipe.groovy

--- copilotpipe.groovy
+++ copilotpipe.groovy
@@ -30,7 +30,7 @@ properties([
             script: [$class: 'GroovyScript',
                 script: [script: '''
                     if (OPERATION == "amhstart") {
-                        return "<b>Please select environments below:</b>"
+                        return "<b>Please select environments below:</b>${environments}"
                     } else {
                         return ""
                     }
