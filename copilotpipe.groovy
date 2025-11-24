86d1870ca4d0eb13b09e82f39b0fc12568f42071
Joinke <38005255+joinke@users.noreply.github.com>
2025-11-24 20:05:48 +1100

    Update copilotpipe.groovy

--- copilotpipe.groovy
+++ copilotpipe.groovy
@@ -1,132 +1,40 @@
-def environments = '["UAT01","UAT02","UAT03"]'
-def test = 'ikke'
-def test2 = '["UAT01": "UAT01","UAT02": "UAT02","UAT03": "UAT03"]'
-properties([
-    parameters([
-        // Dropdown for OPERATION
-        [$class: 'DynamicReferenceParameter',
-            name: 'OPERATION',
-            choiceType: 'ET_FORMATTED_HTML',
-            description: 'Select the operation',
-            script: [$class: 'GroovyScript',
-                script: [script: '''
-                    return """
-                        <b>Choose an operation:</b><br>
-                        <select name='value'>
-                          <option value='amhstart'>Start AMH</option>
-                          <option value='amhstop'>Stop AMH</option>
-                          <option value='amhrestart'>Restart AMH</option>
-                        </select>
-                    """
-                ''', sandbox: true],
-                fallbackScript: [script: 'return "<i>No operations available</i>"', sandbox: true]
-            ]
-        ],
-
-        // Dynamic HTML block for instructions
-        [$class: 'DynamicReferenceParameter',
-            name: '',
-            description: '',
-            choiceType: 'ET_FORMATTED_HTML',
-            referencedParameters: 'OPERATION',
-            script: [$class: 'GroovyScript',
-                script: [script: """
-                    if (OPERATION == "amhstart") {
-                        return "<b>Please select environments below:</b>${test}"
-                    } else {
-                        return ""
-                    }
-                """, sandbox: true],
-                fallbackScript: [script: 'return "<i>No environments available</i>"', sandbox: true]
-            ]
-        ],
-        [
-            $class: 'DynamicReferenceParameter',
-            name: '\u200B',
-            choiceType: 'ET_FORMATTED_HTML',
-            omitValueField: false,
-            referencedParameters: 'OPERATION',
-            script: [
-                $class: 'GroovyScript',
-                script: [
-                    $class: 'SecureGroovyScript',
-                    sandbox: true,
-                    script: '''
-                        def envMap = [
-                            'UAT01': 'UAT01',
-                            'UAT02': 'UAT02',
-                            'UAT03': 'UAT03'
-                        ]
-                        def op = OPERATION?.trim()
-                        def defaultSelected = ['UAT02']
-                        
-                        if (op == 'amhstart') {
-                        // Build checkbox list
-                        
-                        def html = new StringBuilder("<b>Environment</b><br>")
-                        envMap.each { value, label ->
-                            def checked = (value in defaultSelected) ? 'checked' : ''
-                            html.append("<label>")
-                            html.append("<input type='checkbox' name='value' value='${value}' ${checked}> ${label}")
-                            html.append("</label><br>")
-                        }
-                        return html.toString()
-                        } else {
-                          return ''
-                        }
-                    '''
-                ]
-            ]
-        ],
-            [$class: 'DynamicReferenceParameter',
-            name: '\u200C',
-            choiceType: 'ET_FORMATTED_HTML',
-            omitValueField: true,
-            referencedParameters: 'OPERATION',
-            script: [
-                $class: 'GroovyScript',
-                script: [
-                    $class: 'SecureGroovyScript',
-                    sandbox: true,
-                    script: '''
-                        def compMap = [
-                            'STP': 'STP',
-                            'WB': 'WB',
-                            'ALL': 'BOTH'
-                        ]
-                        def op = OPERATION?.trim()
-                        // Pre-select some options if needed
-                        def defaultValue = 'ALL'
-                        if (op == 'amhstart') {
-                        // Build checkbox list
-                        def html = new StringBuilder("<b>Component</b><br><select name='value'>")
-                        compMap.each { value, label ->
-                            def selected = (value == defaultValue) ? 'selected' : ''
-                            html.append("<option value='${value}' ${selected}>${label}</option>")
-                        }
-                        html.append("</select>")
-                        return html.toString()
-                        } else {
-                          return ''
-                        }
-                    '''
-                ]
-            ]
-        ]
-    ])
-])
+def getChoices() {
+    def file = new File("${workspace}/options.txt")
+    return file.readLines()
+}
 
 pipeline {
-  agent any
-  stages {
-    stage('Print Params') {
-      steps {
-        script {
-          echo "OPERATION: ${params['OPERATION']}"
-          echo "ENV: ${params['\u200B']}"
-          echo "COMP: ${params['\u200C']}"
-        }
-      }
-    }
+    agent any
+    options {
+        ansiColor('xterm')
+    } 
+    parameters {
+    choice(name: 'MY_PARAM', choices: getChoices(), description: 'Pick one')
   }
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+                //sh 'cat /opt/concurrent.yml'
+                //sh 'cat /opt/ansible_hosts'
+                ansiColor('xterm') {
+                    withCredentials([sshUserPrivateKey(credentialsId: '00b69538-5290-4373-a385-c2e59e5a4d9f',
+                                               keyFileVariable: 'SSH_KEY',
+                                               usernameVariable: 'SSH_USER')]) {
+                      sh '''
+                        echo "🧩 Using SSH key from Jenkins: $SSH_KEY for user $SSH_USER and $OPERATION and envirionments $SELECTEDENVS and component $SELECTEDCOMP and site $SELECTEDSITE "
+            
+                        # Run the Python wrapper (Ansible will use the key directly)
+                        python3 -u /opt/test.py
+                      '''
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            archiveArtifacts artifacts: 'fetched/**/*'
+        }
+    }
 }
